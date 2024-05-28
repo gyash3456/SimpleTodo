2b423e4d98039ec17dd52a341425a0765ecc1f3b
Yash Gupta <yashgupta@Yashs-MacBook-Air.local>
2024-05-28 19:56:40 +0530

    useDebounce explained

--- src/DebounceThrottle/useDebounce.js
+++ src/DebounceThrottle/useDebounce.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import {useState, useEffect } from 'react'
+import React from "react";
 
-export const useDebounce = (text,delay=1000) => {
- const [value,setValue]= useState(text);
-useEffect(()=>{
-    const timeout=setTimeout(()=>{
-        setValue(text);
-     },delay);
-     return ()=>{clearTimeout(timeout)}
-},[text])
- 
- return value;
-}
+const useDebounce1 = (str, delay = 1000) => {
+  const [text, setText] = React.useState(str); //1
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setText(str);
+    }, delay);
+    return () => clearTimeout(timeout); //2
+  });
+  return text; //3
+};
+//line 1 runs only first time when the page loads,never after that.
+//each time str in parent component is changed the whole component reruns
+//line 3 returns the last text set since setText is never called.
+//since text is a useState therefore it will not change and don't re-render the component
+//what is the role of line 2
+//if we don't write line 2 each time useEffect is called a new settime out is created
+//and all the settimeout will continously fire after 1 second.
+//but when we write clearTimeOut every time new useEffect is called it clears previous timeout
+//until last timeout when cleartimeout is not called and last timeout waits for delay.
+//as soon as last timeout is called setTimout set the state and the parent component is re-rendered to
+//show the effect.
+export default useDebounce1;
